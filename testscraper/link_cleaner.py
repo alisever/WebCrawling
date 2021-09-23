50fbc1d5fc70f00ba929cf67798e6aa67441c79e
Ali Sever <alisever96@hotmail.com>
2021-09-23 21:50:12 +0100

    Finished yenisafak

--- testscraper/link_cleaner.py
+++ testscraper/link_cleaner.py
@@ -1,23 +1,47 @@
-from json import load
+from json import load, dump
 import requests
 
-
-with open('yenisafakhaberleri.json') as json_file:
+with open('old_files/yenisafak_cleaned.json') as json_file:
     news_pages = load(json_file)
-# TODO add these to a json and overwrite the old one
+print(len(news_pages))
+with open('yenisafak.json', 'w') as json_file:
+    dump(news_pages, json_file, indent=4)
+exit()
+
+with open('old_files/yenisafakhaberleri.json') as json_file:
+    news_pages = load(json_file)
+
+with open('old_files/yenisafak_cleaned.csv', 'r') as file:
+    fixed = file.read()
+
+cleaned_pages = []
+
 for index, item in enumerate(news_pages):
-    if index < 5000:
-        continue
     if index % 100 == 0:
         print(index)
     url = 'https://www.yenisafak.com/' + item['link']
-    response = requests.get(url)
-    if response.status_code == 200:
+    if url in fixed:
+        cleaned_pages.append({'link': url, 'type': item['type']})
+        continue
+    try:
+        response = requests.get(url)
+    except requests.exceptions.TooManyRedirects:
         pass
     else:
-        url = 'https://www.yenisafak.com/gundem/' + item['link']
-        response = requests.get(url)
         if response.status_code == 200:
-            pass
-        else:
-            print("Could not find url for", item['link'])
+            cleaned_pages.append({'link': url, 'type': item['type']})
+            continue
+    url = 'https://www.yenisafak.com/gundem/' + item['link']
+    if url in fixed:
+        cleaned_pages.append({'link': url, 'type': item['type']})
+        continue
+    response = requests.get(url)
+    if response.status_code == 200:
+        cleaned_pages.append({'link': url, 'type': item['type']})
+    else:
+        print({'link': url, 'type': item['type']})
+
+cleaned_pages = [dict(t) for t in {tuple(d.items()) for d in cleaned_pages}]
+
+with open('old_files/yenisafak_cleaned.json', 'w') as json_file:
+    dump(cleaned_pages, json_file)
---
+++ testscraper/link_cleaner_threading.py
@@ -0,0 +1,56 @@
+import csv
+import multiprocessing
+from json import load, dumps
+import requests
+import codecs
+
+with codecs.open('old_files/yenisafakhaberleri.json', encoding='utf-8') as json_file:
+    news_pages = load(json_file)
+
+with open('old_files/yenisafak_cleaned.csv', 'r') as file:
+    # fixed = "".join(line for line in file if line.startswith('https'))
+    fixed = file.read()
+
+# with open('yenisafak_cleaned_1.csv', 'w') as fd:
+#     fd.write(fixed)
+
+# with open('yenisafak_cleaned.csv') as result:
+#     uniqlines = set(result.readlines())
+#     with open('yenisafak_cleaned_1.csv', 'w') as rmdup:
+#         rmdup.writelines(set(uniqlines))
+
+
+def write_file(new_data, filename='yenisafak_cleaned.txt'):
+    with open(filename, 'a') as my_file:
+        my_file.write('\n')
+        my_file.write(new_data)
+
+
+def cleaner(argument):
+    index, item = argument
+    if index % 100 == 0:
+        print(index)
+    if item['link'] in fixed:
+        write_file(dumps(item))
+        return None
+    url = 'https://www.yenisafak.com/' + item['link']
+    try:
+        response = requests.get(url)
+    except requests.exceptions.TooManyRedirects:
+        pass
+    else:
+        if response.status_code == 200:
+            write_file(dumps({'link': url, 'type': item['type']}))
+            return None
+
+    url = 'https://www.yenisafak.com/gundem/' + item['link']
+    response = requests.get(url)
+    if response.status_code == 200:
+        write_file(dumps({'link': url, 'type': item['type']}))
+    else:
+        print({'link': url, 'type': item['type']})
+
+
+if __name__ == '__main__':
+    p = multiprocessing.Pool()
+    p.map(cleaner, enumerate(news_pages))
