baa6965bef15d37ebbf058e3c9f7bf3968b04ecd
Ali Sever <alisever96@hotmail.com>
2021-10-12 10:30:36 +0100

    Scraped some stuff

--- testscraper/store_news.py
+++ testscraper/store_news.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
 
-df = pd.read_json('../old_files/Milliyet/milliyet9.json')
-
-df.to_excel('milliyet.xlsx', index=False)
+# df = pd.read_json('../old_files/Milliyet/milliyet9.json')
+#
+# df.to_excel('milliyet.xlsx', index=False)
+df = pd.read_csv('../sara_bareilles.csv')
+print(df['name'])
+df['name'].to_csv('../sara_songs.csv', index=False)
